df9c27613abaf1d172694b312c18523f92d80402
Jendrik Seipp <jendrik.seipp@liu.se>
2023-06-05 10:57:59 +0200

    Use 16GB for test runs.

--- experiments/2023-06-04-test-learning.py
+++ experiments/2023-06-04-test-learning.py
@@ -11,13 +11,13 @@ import tracks
 
 TRACK = tracks.SINGLE_CORE
 TIME_LIMIT = 10 * 60 * 60  # seconds
-MEMORY_LIMIT = 90 * 1024  # MiB
+MEMORY_LIMIT = 16 * 1024  # MiB
 
 if learning_experiment.running_on_cluster():
     TESTRUN = False
     ENVIRONMENT = TetralithEnvironment(
         email="jendrik.seipp@liu.se",
-        memory_per_cpu="91G",
+        memory_per_cpu="17G",
         cpus_per_task=1,
         time_limit_per_task="11:00:00",  # 10 planners * 10 domains = 100 runs, so only one run per Slurm task.
         export=["PATH"],
