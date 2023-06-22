547a6e4337f8a8584b28dd7f5cdcd462d18110f6
Jendrik Seipp <jendrik.seipp@liu.se>
2023-06-22 13:47:55 +0200

    Only keep first and last parts of large files.

--- experiments/sort-logs.py
+++ experiments/sort-logs.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 
 import argparse
+import io
+import os
 from pathlib import Path
 import shutil
-from subprocess import check_call
+import tarfile
 
 from lab.tools import Properties
+
+MAX_FILE_SIZE = 50 * 1024  # 500 KiB
+PART_SIZE = MAX_FILE_SIZE // 2
+
+
+def tar_add_large_files(tar, filepath, arcname):
+    filesize = filepath.stat().st_size
+    if filesize > MAX_FILE_SIZE:
+        print(f"File too large: {filepath}")
+        with open(filepath, 'rb') as f:
+            first_part = f.read(PART_SIZE)
+            f.seek(-PART_SIZE, os.SEEK_END)
+            last_part = f.read(PART_SIZE)
+        buffer = first_part + b"\n\n\n\n\n ===== FILE TOO LARGE --> MIDDLE FILE CONTENTS OMITTED ===== \n\n\n\n\n" +  last_part
+        fileobj = io.BytesIO(buffer)
+        file_obj = tarfile.TarInfo(name=arcname)
+        file_obj.size = len(buffer)
+        tar.addfile(file_obj, fileobj=fileobj)
+    else:
+        tar.add(filepath, arcname=arcname)
 
 
 def sort_run_dir(run_dir, logs_dir):
@@ -17,12 +39,21 @@ def sort_run_dir(run_dir, logs_dir):
     target.mkdir(parents=True, exist_ok=True)
     uncompressed_files = {"properties", "static-properties", "driver.log", "run", "run.err", "run.log", "values.log", "watch.log"}
     compressed_files = []
-    for f in run_dir.glob("*"):
-        if f.name not in uncompressed_files:
-            compressed_files.append(f)
+
+    for root, _, files in os.walk(run_dir):
+        for file in files:
+            filepath = Path(root) / file
+            if filepath.name not in uncompressed_files:
+                compressed_files.append(filepath)
+
     tar_filename = target / "other_files.tgz"
-    tar_cmd = ["tar", "-czf", str(tar_filename)] + [f.name for f in compressed_files]
-    check_call(tar_cmd, cwd=run_dir)
+
+    with tarfile.open(tar_filename, 'w:gz') as tar:
+        for f in compressed_files:
+            rel_path = os.path.relpath(f, run_dir)
+            arcname = f"{rel_path}"
+            tar_add_large_files(tar, f, arcname)
+
     for path in uncompressed_files:
         if (run_dir / path).exists():
             shutil.copy2(run_dir / path, target)
